bf277d41f98bb575fd6a84f57f0cee8edad7568e
kevinmaulanan <kevin.maulana@codr.id>
2023-09-29 16:03:12 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import MetaTags from "react-meta-tags";
 
 function App() {
   return (
     <div className="App">
+      <MetaTags>
+        <meta charset="UTF-8" />
+        <meta name="description" content="Zeta and Kavi" />
+        <meta
+          name="keywords"
+          content="Zeta Kavi, Kavi Zeta, Testing Kavi Zeta, Kavi Testing Zeta, Zeta Testing Kavi"
+        />
+        <meta name="author" content="Kavi" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </MetaTags>
+      <div class="content"> Some Content </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -13,8 +25,7 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
